2da04c5b5a6951c3bb2f167151ab5019d14fbbfc
huthaifa17 <33427217+huthaifa17@users.noreply.github.com>
2018-01-04 22:27:45 -0800

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -4,5 +4,15 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Bot {
+    private String name;
+    priavte int age;
     
+    public Bot(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    public String toString()
+    {
+        return "Hi"+name"!"
+    }
 }
